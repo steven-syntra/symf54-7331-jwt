15e45af0b0bbc88b88f47834b85fd247c7e28f2b
steven-syntra <info@inform.be>
2022-06-02 00:34:55 +0200

    commit einde voorbereiding les 3/6/2022

--- src/Entity/Teacher.php
+++ src/Entity/Teacher.php
@@ -4,10 +4,22 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\TeacherRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     normalizationContext={
+ *     "groups"={"teachers:read", "teachers:write", "vakken:read"},
+ *     "enable_max_depth"=true
+ *     },
+ *     denormalizationContext={
+ *     "groups"={"teachers:write"}
+ *     }
+ * )
  * @ORM\Entity(repositoryClass=TeacherRepository::class)
  */
 class Teacher
@@ -16,28 +28,47 @@ class Teacher
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"teachers:read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"teachers:read", "teachers:write","vakken:read"})
      */
     private $naam;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups({"teachers:read", "teachers:write","vakken:read"})
      */
     private $voornaam;
 
     /**
      * @ORM\Column(type="date", nullable=true)
+     * @Groups({"teachers:read", "teachers:write", "vakken:read"})
      */
     private $geboortedatum;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups({"teachers:read", "teachers:write"})
      */
     private $specialisatie;
+
+    /**
+     * @ORM\OneToMany(targetEntity=VakTeacher::class, mappedBy="teacher", orphanRemoval=true)
+     * @Groups({"teachers:read","teachers:write"})
+     * @MaxDepth(1)
+     */
+    private $vakken;
+
+
+
+    public function __construct()
+    {
+        $this->vakken = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -88,6 +119,36 @@ class Teacher
     public function setSpecialisatie(?string $specialisatie): self
     {
         $this->specialisatie = $specialisatie;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, VakTeacher>
+     */
+    public function getVakken(): Collection
+    {
+        return $this->vakken;
+    }
+
+    public function addVakken(VakTeacher $vakken): self
+    {
+        if (!$this->vakken->contains($vakken)) {
+            $this->vakken[] = $vakken;
+            $vakken->setTeacher($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVakken(VakTeacher $vakken): self
+    {
+        if ($this->vakken->removeElement($vakken)) {
+            // set the owning side to null (unless already changed)
+            if ($vakken->getTeacher() === $this) {
+                $vakken->setTeacher(null);
+            }
+        }
 
         return $this;
     }
